f1c77e4a35fe9e30c1fe142487ec55b23b1918cc
patrykpl2612 <43454785+patrykpl2612@users.noreply.github.com>
2021-06-25 09:00:23 +0200

    ZadanieRekrutacyjne1

---
+++ zadannieRekrutacyjne/ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Infrastructure;
+using ToDoList.Models;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using System.Data;
+using System.Configuration;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext context;
+
+        public ToDoController(ToDoContext context)
+        {
+            this.context = context;
+        }
+
+        // GET /
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.ToDoList orderby i.Id select i;
+
+            List<TodoList> todoList = await items.ToListAsync();
+
+            return View(todoList);
+
+        }
+
+        // GET /todo/create
+        public IActionResult Create() => View();
+
+        // POST /todo/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The item has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+
+        }
+
+        // GET /todo/edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+
+        }
+
+        // POST /todo/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The item has been uppdated!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+
+        }
+
+        // GET /todo/delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                TempData["Error"] = "The item does not exist!";
+            }
+            else
+            {
+                context.ToDoList.Remove(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The item has been deleted!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET /todo/ShowSpecific
+        public async Task<ActionResult> ShowSpecific(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+
+            return View(item);
+
+        }
+        
+        // PDF Export
+        public IActionResult Test()
+        {
+            db dpob = new db();
+
+            var document = new Document
+            {
+                PageInfo = new PageInfo { Margin = new MarginInfo(28,28,28,40) }
+            };
+            var pdfpage = document.Pages.Add();
+            Table table = new Table
+            {
+                ColumnWidths = "25% 25%",
+                DefaultCellPadding = new MarginInfo(10, 5, 10, 5),
+                Border = new BorderInfo(BorderSide.All, .5f, Color.Black),
+                DefaultCellBorder = new BorderInfo(BorderSide.All, .2f, Color.Black),
+            };
+
+            DataTable dt = dpob.GetRecord();
+            table.ImportDataTable(dt, true, 0, 0);
+            document.Pages[1].Paragraphs.Add(table);
+
+            using (var streamout = new MemoryStream())
+            {
+                document.Save(streamout);
+                return new FileContentResult(streamout.ToArray(), "application/pdf") 
+                {
+                    FileDownloadName = "TasksToDo.pdf"
+                };
+            }
+        }
+
+        public async Task<ActionResult> ExportSelected()
+        {
+            IQueryable<TodoList> items = from i in context.ToDoList orderby i.Id select i;
+
+            List<TodoList> todoList = await items.ToListAsync();
+
+            return View(todoList);
+
+        }
+
+        [HttpPost]
+        public JsonResult ExportSelectedToPDFJson(string ItemList)
+        {
+            string[] arr = ItemList.Split(',');
+
+            foreach (var id in arr)
+            {
+                var currentId = id;
+            }
+
+            return Json("");
+        }
+
+
+
+        public IActionResult ExportSelectedToPDF(string ItemList)
+        {
+            db dpob = new db();
+
+            var document = new Document
+            {
+                PageInfo = new PageInfo { Margin = new MarginInfo(28, 28, 28, 40) }
+            };
+            var pdfpage = document.Pages.Add();
+            Table table = new Table
+            {
+                ColumnWidths = "25% 25%",
+                DefaultCellPadding = new MarginInfo(10, 5, 10, 5),
+                Border = new BorderInfo(BorderSide.All, .5f, Color.Black),
+                DefaultCellBorder = new BorderInfo(BorderSide.All, .2f, Color.Black),
+            };
+
+            
+
+            DataTable dt = dpob.GetSelectedRecords();
+            table.ImportDataTable(dt, true, 0, 0);
+            document.Pages[1].Paragraphs.Add(table);
+
+            using (var streamout = new MemoryStream())
+            {
+                document.Save(streamout);
+                return new FileContentResult(streamout.ToArray(), "application/pdf")
+                {
+                    FileDownloadName = "TasksToDo.pdf"
+                };
+            }
+        }
+
+    }
+}
---
+++ zadannieRekrutacyjne/ToDoList/Infrastructure/db.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ToDoList.Infrastructure
+{
+    public class db
+    {
+        SqlConnection con = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=ToDoContext;Trusted_Connection=True;MultipleActiveResultSets=true");
+        public DataTable GetRecord()
+        {
+            SqlCommand com = new SqlCommand("select * from ToDoList", con);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public DataTable GetSelectedRecords()
+        {
+            SqlCommand com = new SqlCommand("select * from ToDoList", con);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+    }
+}
